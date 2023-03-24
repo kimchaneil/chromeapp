5c08a645e8f43f83cc989d2a018882e64a7a1efd
kimchaneil <chaneil2@naver.com>
2023-03-25 00:49:16 +0900

    4.1

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button"); //이러한 방식도 가능하지만 애초에 const loginButton = document.queryselector("#login-form button")의 형태도 가능
 
 function onLoginBtnClick(){
-    console.dir(loginInput)
-    console.log("hello",loginInput.value)
+    const usrname = loginInput.value;
+    if( usrname === ""){
+        alert("plz write your name");
+
+    }else if(usrname.length >= 15){
+        alert("ur name is too long");
+    }
 }
 loginButton.addEventListener("click",onLoginBtnClick)
