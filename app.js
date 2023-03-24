a395cbac599dfcab149ff0a1119371353cc25f70
kimchaneil <chaneil2@naver.com>
2023-03-25 01:04:09 +0900

    4.2

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button"); //이러한 방식도 가능하지만 애초에 const loginButton = document.queryselector("#login-form button")의 형태도 가능
 
-function onLoginBtnClick(){
-    const usrname = loginInput.value;
-    if( usrname === ""){
-        alert("plz write your name");
-
-    }else if(usrname.length >= 15){
-        alert("ur name is too long");
-    }
+function onLoginSubmit(event){
+    event.preventDefault(); //자동으로 브라우저에서 새로고침을 하는 것을 방지
+    console.log(loginInput.value);
 }
-loginButton.addEventListener("click",onLoginBtnClick)
+loginForm.addEventListener("submit",onLoginSubmit)
