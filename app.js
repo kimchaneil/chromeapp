5ac2c90a474b1a0396687b5a3a19c508562ca616
kimchaneil <chaneil2@naver.com>
2023-03-25 01:42:49 +0900

    4.5-6

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ const HIDDEN_CLASSNAME ="hidden"
 function onLoginSubmit(event){
     event.preventDefault(); //자동으로 브라우저에서 새로고침을 하는 것을 방지
     const username = loginInput.value;
+    localStorage.setItem("usrname",username) //saving username to local storage key"usrname"
     loginForm.classList.add(HIDDEN_CLASSNAME)
     greeting.innerText ="Hello " + username;
-    greeting.innerText =`Hello ${username}`;
+    greeting.innerText =`Hello ${username}`; // ' 가아닌 `임
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
+const savedName = localStorage.getItem("usrname")
+console.log(savedName);
+if(savedName === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    //show the greetings 
+    greeting.innerText = `Hello ${savedName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+   
+}
