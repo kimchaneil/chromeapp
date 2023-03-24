5606f138f330959941478974c450041ae053d8e4
kimchaneil <chaneil2@naver.com>
2023-03-25 01:30:56 +0900

    4.4

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button"); //이러한 방식도 가능하지만 애초에 const loginButton = document.queryselector("#login-form button")의 형태도 가능
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("#login-form a");
-
+const HIDDEN_CLASSNAME ="hidden"
 function onLoginSubmit(event){
     event.preventDefault(); //자동으로 브라우저에서 새로고침을 하는 것을 방지
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    greeting.innerText ="Hello " + username;
+    greeting.innerText =`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-    
-    //alert("clicked!"); //alert가 이후 동작을 막는다
-}
+
 loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
