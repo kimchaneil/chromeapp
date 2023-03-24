eff0633bc391a1aee4a3c0f9040e0363f22eb3cc
kimchaneil <chaneil2@naver.com>
2023-03-25 04:05:46 +0900

    7.3_saveToDo

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,10 @@ const toDOForm = document.getElementById("todo-form");
 const toDOInput = toDOForm.querySelector("input"); //document.querySelector("#todo-form input")
 const toDOList = document.getElementById("todo-list");
 
+const toDos = [];
+function savetoDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos)); //JSON stringify는 문자열로 저장을 해주는 기능을 가짐
+}
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -23,8 +27,10 @@ function handleToDoSubmit(event) {
     const newTodo = toDOInput.value;
     console.log(toDOInput.value);
     toDOInput.value = "";
+    toDos.push(newTodo);
     //console.log(newTodo,toDOInput.value);
     paintToDo(newTodo);
+    savetoDos();
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
