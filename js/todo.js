8ae7d155eea467fe825908523124cc8cbd3c08a1
kimchaneil <chaneil2@naver.com>
2023-03-25 03:47:21 +0900

    7.1_addtodo

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,21 @@ const toDOForm = document.getElementById("todo-form");
 const toDOInput = toDOForm.querySelector("input"); //document.querySelector("#todo-form input")
 const toDOList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDOList.appendChild(li);
+    console.log(li);
+}
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDOInput.value;
     console.log(toDOInput.value);
     toDOInput.value = "";
-    console.log(newTodo,toDOInput.value);
+    //console.log(newTodo,toDOInput.value);
+    paintToDo(newTodo);
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
